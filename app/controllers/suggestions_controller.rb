7c9671c850454c233a1fc785dc138d5763f5c479
sophia <sophiabaldonado@gmail.com>
2016-05-17 16:12:34 -0700

    Redirect to home page if search returns nil;

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -3,8 +3,13 @@
 class SuggestionsController < ApplicationController
   def index
     # @favorites = # TunesTakeoutWrapper.favorites
-    @tunes_takeout = ::TunesTakeoutWrapper.search(params["search"])
+    @tunes_takeout = TunesTakeoutWrapper.search(params["search"])
     @suggestions = @tunes_takeout.suggestions
+    if @suggestions.nil?
+      redirect_to root_path, notice: "No matches for #{params["search"]}!"
+    else
+      render :index
+    end
     # raise
     # @suggestion = TunesTakeoutWrapper.search(params[:query])
   end
