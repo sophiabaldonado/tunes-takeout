54ed7fc316ecb5ec4dcd8bb122b6ed7725d6b59f
sophia <sophiabaldonado@gmail.com>
2016-05-17 20:14:55 -0700

    Sign in with Spotify creates a User correctly;

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,20 @@
 class User < ActiveRecord::Base
+  validates :name, :uid, :provider, presence: true
+
   def self.find_or_create_from_omniauth(auth_hash)
-  # Find or create a user
-  user = User.find_by(uid: auth_hash.uid, provider: auth_hash[:provider])
-  if !user.nil?
-    return user
-  else
-    user = User.new(name: auth_hash[:info][:name], uid: auth_hash[:info][:id], provider: auth_hash[:provider])
-    user.save
-    if user.save
+    # Find or create a user
+    user = self.find_by(uid: auth_hash["info"]["id"], provider: auth_hash[:provider])
+    if !user.nil?
       return user
     else
-      return nil
+      user = User.new(name: auth_hash["info"]["display_name"], uid: auth_hash["info"]["id"], provider: auth_hash[:provider])
+
+      if user.save
+        return user
+      else
+        return nil
+      end
     end
   end
-end
 
 end
