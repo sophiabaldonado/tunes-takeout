e9399a521cd6093ab97a49a6206a69cf19a9a48e
sophia <sophiabaldonado@gmail.com>
2016-05-19 15:53:46 -0700

    Create and use method to see if a user has favorited a given suggestion;

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  helper_method :current_user
+  helper_method :current_user, :user_favorited?
 
   protect_from_forgery with: :exception
 
@@ -16,6 +16,22 @@ class ApplicationController < ActionController::Base
         music: Music.suggested_music(sug),
         food: Food.suggested_food(sug)
       }
+    end
+  end
+
+  def user_favorited?(suggestion_id)
+    @favorites = TunesTakeoutWrapper.favorites(current_user.uid)
+    @favorites.include? suggestion_id
+  end
+
+  def choose_correct_suggestions
+    if params[:search]
+      @tunes_takeout_suggestions = TunesTakeoutWrapper.search(params[:search], params[:limit]).suggestions
+      @suggestions = sort_suggestions(@tunes_takeout_suggestions)
+    else
+      @top_suggestions_ids = TunesTakeoutWrapper.top_twenty
+      @top_suggestions = TunesTakeoutWrapper.find_many(@top_suggestions_ids).suggestions
+      @suggestions = sort_suggestions(@top_suggestions)
     end
   end
 
